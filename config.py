8b69ebf53e0331532392b1bd87f05fdc5b039ef7
aditya <adityasu12@gmail.com>
2021-02-10 15:51:44 +0530

    created config file

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+basePath = '../data'
+lr=1e-4
+
+savePath = 'savedModel'
+
+os.makedirs(savePath, exist_ok=True)
+
+seed = 0
+numEpochs = 10
+
+batchSize = {
+    'train': 20,
+    'test': 40
+}
+
+numWorkers = {
+    'train': 2,
+    'test': 2
+}
+
+iterations = {
+    'train': 1000,
+    'test': 100
+}
