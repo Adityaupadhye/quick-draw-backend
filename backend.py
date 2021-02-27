5b36589d49cbc237dacb17d8b4051a6a5ad97b8d
aditya <adityasu12@gmail.com>
2021-02-27 23:15:29 +0530

    added result route for getting image form frontend and running model on it

--- backend.py
+++ backend.py
@@ -3,11 +3,42 @@ from flask import Flask, request
 import json
 from flask_cors import CORS
 import base64
+import runmodel
+from datetime import datetime as dt
 
 # initialize flask app
 app = Flask(__name__)
 cors = CORS(app)
 datasetPath = 'data'
+
+
+def findtime():
+    now = dt.now()
+    return now.strftime('%d%m%Y_%H%M%S')
+
+
+@app.route('/api/test')
+def test_fun():
+    return "this is test function"
+
+
+@app.route('/api/result', methods=['POST'])
+def result():
+    data = json.loads(request.data.decode('utf-8'))
+    print(type(data))
+    image = data['url'].split(',')[1].encode('utf-8')
+    # print('got this url= ', imgurl)
+
+    os.makedirs('userdata', exist_ok=True)
+    filename = findtime()+'.png'
+
+    with open(f'userdata/{filename}', 'wb') as f:
+        f.write(base64.decodebytes(image))
+
+    output = runmodel.test(f'userdata/{filename}')
+    print(output)
+
+    return 'got image url ' + output
 
 
 @app.route('/upload_canvas', methods=['POST'])
